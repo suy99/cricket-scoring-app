89ea06619b152a1c222b34922b76d24fe94d148f
Suyog Choure <excelsuyog@gmail.com>
2025-08-19 20:48:50 +0530

    Update all screens to be stateless and navigation-friendly. Use MainNavigation for navigation bar and top app bar.

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,18 +3,17 @@ package com.cricket.cricscore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.navigation.compose.rememberNavController
+import com.cricket.cricscore.ui.MainNavigation
+import com.cricket.cricscore.ui.theme.CricketScoringAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                color = MaterialTheme.colorScheme.background
-            ) {
-                Text("Welcome to Cricket Scoring App!")
+            CricketScoringAppTheme {
+                val navController = rememberNavController()
+                MainNavigation(navController)
             }
         }
     }
